1311a9c953f9ed834c8a2db6ceeb2153963aede2
cdspurlock <107364140+cdspurlock@users.noreply.github.com>
2022-07-17 00:11:08 -0500

    Add files via upload
    This is a freecodecamp.org project using basic JavaScript to change the background color and to give color code in hex value or another value.

---
+++ Color_Flipper/app.js
@@ -0,0 +1,14 @@
+let colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+let btn = document.getElementById("btn");
+let color = document.querySelector(".color");
+
+btn.addEventListener('click', function(e) 
+{
+    let randomNumber = getRandomNumber();
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length);
+}
